25905c22d95a5e687ccfc0e20037337d6c18b063
Chris Johnson and Connor miller <connor@>
2017-10-17 15:52:46 -0700

    add sound file and random score generator and loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,23 @@
 $(document).ready(function() {
+  var battle = new Audio("battle.mp3");
+  $("button#battle-beasts").click(function() {
+    function demo() {
+      battle.play();
+      //await sleep(1000);
+      if (odds > 0.5) {
+        console.log('dog win');
+        $("ul#dog").prepend("<li class='list-group-item'>Woof Woof!</li>")
+      } else {
+        $("ul#cat").prepend("<li class='list-group-item'>Meow Meow!</li>")
+        console.log('cat win');
+      }
+    }
+    for (var i = 0; i < 20; i++){
+      console.log(odds)
+      var odds = Math.random();
+      demo();
+    }
+  });
   $("button#dog-button").click(function() {
     $("ul#dog").prepend("<li class='list-group-item'>Woof Woof!</li>")
   });
